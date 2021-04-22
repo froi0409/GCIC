ea94a9beac8b2b4fa09b4364c580a563fe715978
Fernando Ocaña <ferrubocana@gmail.com>
2021-04-22 01:12:08 -0600

    El editor de texto ha sido trasladado a una aplicación web, el mismo ya cuenta con las funciones de importación y exportación de archivos

---
+++ GCIC/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index.jsp
+    Created on : 21/04/2021, 20:02:38
+    Author     : froi-pc
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Inicio</title>
+    </head>
+    <body>
+        <% response.sendRedirect("editor.jsp");             %>
+    </body>
+</html>
---
+++ GCIC/src/main/webapp/js/funciones.js
@@ -0,0 +1,55 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+const input = document.getElementById('archivo');
+const editor = document.getElementById('entrada');
+input.addEventListener('change', function () {
+    console.log('hola mundo');
+    if (input.files.length > 0) {
+        readFile(input.files[0]);
+    }
+});
+
+function readFile(file) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        editor.value= reader.result; 
+    }
+    reader.readAsText(file);
+}
+
+function actualizarPosicion() {
+    var txtEntrada = document.getElementById('entrada');
+    var textLines = txtEntrada.value.substr(0, txtEntrada.selectionStart).split("\n");
+    var currentLine = textLines.length;
+    var currentColumn = textLines[textLines.length-1].length + 1;
+    var texto = 'Linea: ' + currentLine + " - Columna: " + currentColumn;
+    document.getElementById('lblPosicion').innerHTML = texto;
+}
+
+function saveTextAsFile(textToWrite, fileNameToSaveAs){
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+            // Chrome allows the link to be clicked
+            // without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+            // Firefox requires the link to be added to the DOM
+            // before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
